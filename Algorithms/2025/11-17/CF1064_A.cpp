247f664ff13e3e13e916c44f8ce44c22ac759ae9
LouissMa <634688345@qq.com>
2025-11-17 14:03:15 +0800

    Add CF1064 (div2) solutions for 2025-11-17

---
+++ Algorithms/2025/11-17/CF1064_A.cpp
@@ -0,0 +1,43 @@
+/*
+Problem: Problem Title
+Contest: Contest Name
+URL: URL
+Memory Limit: 1024 MB
+Time Limit: 2000 ms
+
+Description:
+Paste problem statement here
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define SPEED ios::sync_with_stdio(false);cin.tie(0); cout.tie(0);
+typedef pair<int, int> pii;
+
+void solve() 
+{
+    int n;
+    cin>>n;
+    string s;
+    cin>>s;
+    int ans = 0;
+    for(int i=n-2;i>=0;i--)
+    {
+        if(s[i] != s[n-1]) ans++;
+    }
+    cout<<ans<<"\n";
+    return;
+}
+
+signed main() 
+{
+    SPEED;
+    int t = 1;
+    cin >> t;
+    while(t--) 
+    {
+        solve();
+    }
+    return 0;
+}
---
+++ Algorithms/2025/11-17/CF1064_B.cpp
@@ -0,0 +1,36 @@
+/*
+Problem: Problem Title
+Contest: Contest Name
+URL: URL
+Memory Limit: 1024 MB
+Time Limit: 2000 ms
+
+Description:
+Paste problem statement here
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define SPEED ios::sync_with_stdio(false);cin.tie(0); cout.tie(0);
+typedef pair<int, int> pii;
+
+void solve() 
+{
+    int a,b,n;
+    cin>>a>>b>>n;
+    if(a == b || b <= a / n) cout<<1<<endl;
+    else cout<<2<<endl;
+}
+
+signed main() 
+{
+    SPEED;
+    int t = 1;
+    cin >> t;
+    while(t--) 
+    {
+        solve();
+    }
+    return 0;
+}
