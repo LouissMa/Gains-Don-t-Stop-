c119bae4145ba75ad652c0ba641779d0596e5b4e
LouissMa <634688345@qq.com>
2025-12-07 19:39:04 +0800

    Add Abc358 A solution for 2025-12-07

---
+++ Algorithms/2025/12-07/Abc358_A.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define SPEED ios::sync_with_stdio(false);cin.tie(0); cout.tie(0);
+typedef pair<int, int> PII;
+
+signed main()
+{
+    SPEED;
+    string s,t;
+    cin>>s>>t;
+    if(s == "AtCoder" && t == "Land") cout<<"Yes\n";
+    else cout<<"No\n";
+    return 0;
+}
