4d295b659baa3496301ba101e49d03c8d0c0072c
LouissMa <634688345@qq.com>
2025-12-29 15:59:50 +0800

    Add Nowcoder 124 B solution for 2025-12-29

---
+++ Algorithms/2025/12-29/Nowcoder_124_B.cpp
@@ -0,0 +1,41 @@
+/*
+Problem: Problem Title
+Contest: Contest Name
+URL: URL
+Memory Limit: 1024 MB
+Time Limit: 2000 ms
+
+Description:
+Paste problem statement here
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define SPEED ios::sync_with_stdio(false);cin.tie(0); cout.tie(0);
+typedef pair<int, int> pii;
+
+void solve() 
+{
+    int n;
+    cin>>n;
+    vector<int> a(n);
+    for(int i=0;i<n;i++) cin>>a[i];
+    sort(a.begin(),a.end(),greater<int>());
+    for(int i=0;i<n;i++)
+    {
+        cout<<a[i]<<" \n"[i==n-1];
+    }
+}
+
+signed main() 
+{
+    SPEED;
+    int t = 1;
+    cin >> t;
+    while(t--) 
+    {
+        solve();
+    }
+    return 0;
+}
