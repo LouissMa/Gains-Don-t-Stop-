f5e7dc7662377bbbae4a343afcec26ab07978c0d
LouissMa <634688345@qq.com>
2026-01-01 08:43:53 +0800

    Add ECF186(div2) A solution - First training of 2026 🎊

---
+++ Algorithms/2026/01-01/ECF186_A.cpp
@@ -0,0 +1,44 @@
+/*
+Problem: Problem Title
+Contest: Contest Name
+URL: URL
+Memory Limit: 1024 MB
+Time Limit: 2000 ms
+
+Description:
+Paste problem statement here
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define SPEED ios::sync_with_stdio(false);cin.tie(0); cout.tie(0);
+typedef pair<int, int> pii;
+
+void solve() 
+{
+    int n;
+    cin>>n;
+    string s;
+    cin>>s;
+    if(s.find("2026") != string::npos || s.find("2025") == string::npos)
+    {
+        cout<<0<<endl;
+    }
+    else
+    {
+        cout<<1<<endl;
+    }
+}
+
+signed main() 
+{
+    SPEED;
+    int t = 1;
+    cin >> t;
+    while(t--) 
+    {
+        solve();
+    }
+    return 0;
+}
