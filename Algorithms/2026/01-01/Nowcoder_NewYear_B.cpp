35200ae39425fa70f542c5ce6da6c4f7c117d38b
LouissMa <634688345@qq.com>
2026-01-01 08:57:09 +0800

    Add Nowcoder New Year contest A, B solutions - 2026 New Year special 🎇\\

---
+++ Algorithms/2026/01-01/Nowcoder_NewYear_B.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define SPEED ios::sync_with_stdio(false);cin.tie(0); cout.tie(0);
+typedef pair<int, int> PII;
+
+signed main()
+{
+    SPEED;
+    string s;
+    cin>>s;
+    cout<<"I hope you can go to the Stanford this year!\n";
+    return 0;
+}
