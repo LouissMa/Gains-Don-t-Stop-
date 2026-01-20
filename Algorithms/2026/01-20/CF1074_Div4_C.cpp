9ddd98ce318ce8261305d6c37f7632582c0fedcd
LouissMa <634688345@qq.com>
2026-01-20 16:49:26 +0800

    Add Codeforces 1074 Div4 Problem C solution

---
+++ Algorithms/2026/01-20/CF1074_Div4_C.cpp
@@ -0,0 +1,57 @@
+/*
+Problem: Problem Title
+Contest: Contest Name
+URL: URL
+Memory Limit: 1024 MB
+Time Limit: 2000 ms
+
+Description:
+Paste problem statement here
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define SPEED ios::sync_with_stdio(false);cin.tie(0); cout.tie(0);
+typedef pair<int, int> pii;
+
+void solve() 
+{
+    int n;cin>>n;
+    vector<int> a(n);
+    for(int i=0;i<n;i++) cin>>a[i];
+    sort(a.begin(),a.end());
+    int j =1;
+    for(int i=1;i<n;i++)
+    {
+        if(a[i] != a[i-1])
+        {
+            a[j] = a[i];
+            j++;
+        }
+    }
+    int ans = 1;
+    int c = 1;
+    for(int i=0;i<n;i++)
+    {
+        if(a[i] == a[i -1] + 1)
+        {
+            c++;
+        }
+        else c = 1;
+        ans = max(ans,c);
+    }
+    cout<<ans<<endl;
+}
+
+signed main() 
+{
+    SPEED;
+    int t = 1;
+    cin >> t;
+    while(t--) 
+    {
+        solve();
+    }
+    return 0;
+}
