74b8e85d13e3ee7544e30d7503911e11081cb10f
LouissMa <634688345@qq.com>
2026-01-20 16:44:31 +0800

    Add Codeforces 1074 Div4 Problem A solution for 2026-01-20

---
+++ Algorithms/2026/01-20/CF1074_Div4_A.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+	ios::sync_with_stdio(false),cin.tie(0);
+	int T,n,i;
+	for(cin>>T;T>0;T--)
+	{
+		cin>>n;
+		for(i=1;i<=n;i++)cout<<i<<' ';
+		cout<<'\n';
+	}
+	return 0;
+}
