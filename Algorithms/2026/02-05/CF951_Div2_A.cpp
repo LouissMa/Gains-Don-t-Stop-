e517d88028a5e9f2d82615a90cfad7cee1ac47db
LouissMa <634688345@qq.com>
2026-02-05 11:04:55 +0800

    Add Codeforces 951 Div2 Problem A solution for 2026-02-05

---
+++ Algorithms/2026/02-05/CF951_Div2_A.cpp
@@ -0,0 +1,42 @@
+/*
+Problem: Problem Title
+Contest: Contest Name
+URL: URL
+Memory Limit: 1024 MB
+Time Limit: 2000 ms
+
+Description:
+Paste problem statement here
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define SPEED ios::sync_with_stdio(false);cin.tie(0); cout.tie(0);
+typedef pair<int, int> pii;
+
+void solve() 
+{
+    int n;
+    cin>>n;
+    vector<int> a(n);
+    for(int i=0;i<n;i++) cin>>a[i];
+    int ans = 1e19;
+    for(int i=1;i<n;i++)
+    {
+        ans = min(ans,max(a[i],a[i-1]));
+    }
+    cout<<ans-1<<endl;
+}
+
+signed main() 
+{
+    SPEED;
+    int t = 1;
+    cin >> t;
+    while(t--) 
+    {
+        solve();
+    }
+    return 0;
+}
