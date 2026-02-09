00245cc74426ced759788e3da834078a59ee4d91
LouissMa <634688345@qq.com>
2026-02-09 15:13:38 +0800

    Add Codeforces 1078 Div2 Problem A solution for 2026-02-08

---
+++ Algorithms/2026/02-08/CF1078_Div2_A.cpp
@@ -0,0 +1,35 @@
+/*
+Problem: Problem Title
+Contest: Contest Name
+URL: URL
+Memory Limit: 1024 MB
+Time Limit: 2000 ms
+
+Description:
+Paste problem statement here
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define SPEED ios::sync_with_stdio(false);cin.tie(0); cout.tie(0);
+typedef pair<int, int> pii;
+
+void solve() 
+{
+    int n,w;
+    cin>>n>>w;
+    cout<<n - n / w <<"\n";
+}
+
+signed main() 
+{
+    SPEED;
+    int t = 1;
+    cin >> t;
+    while(t--) 
+    {
+        solve();
+    }
+    return 0;
+}
