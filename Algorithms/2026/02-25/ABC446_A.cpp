cc092352be39f282364f9cc42fb6a85dc9002da0
LouissMa <634688345@qq.com>
2026-02-25 10:45:41 +0800

    Add AtCoder Beginner Contest 446 Problem A solution for 2026-02-25

---
+++ Algorithms/2026/02-25/ABC446_A.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define SPEED ios::sync_with_stdio(false);cin.tie(0); cout.tie(0);
+typedef pair<int, int> PII;
+
+signed main()
+{
+    SPEED;
+    string s;
+    cin>>s;
+    s[0] = s[0] + 'a' - 'A';
+    cout<<"Of";
+    cout<<s<<endl;
+    return 0;   
+}
