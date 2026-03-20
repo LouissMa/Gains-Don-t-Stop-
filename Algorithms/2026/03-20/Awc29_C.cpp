f1dbbff8e2f52e5ae1c1aa49e4c74a093090b563
LouissMa <634688345@qq.com>
2026-03-20 22:55:32 +0800

    Add Awc29 Problem C solution

---
+++ Algorithms/2026/03-20/Awc29_C.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define SPEED ios::sync_with_stdio(false);cin.tie(0); cout.tie(0);
+typedef pair<int, int> PII;
+
+signed main()
+{
+    SPEED;
+    int n;
+    cin>>n;
+    vector<int> a(n);
+    for(int i=0;i<n;i++) cin>>a[i];
+    sort(a.begin(),a.end(),greater<int>());
+    cout<<a[0] + a[1]<<endl;
+    return 0;
+}
