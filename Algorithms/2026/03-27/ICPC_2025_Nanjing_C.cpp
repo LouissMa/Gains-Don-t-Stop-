0f8a83b522143e959db70f549dddcd32f16a0e60
LouissMa <634688345@qq.com>
2026-03-27 22:16:32 +0800

    Add 2025 ICPC Nanjing Problem C solution

---
+++ Algorithms/2026/03-27/ICPC_2025_Nanjing_C.cpp
@@ -0,0 +1,43 @@
+/*
+Problem: Problem Title
+Contest: Contest Name
+URL: URL
+Memory Limit: 1024 MB
+Time Limit: 2000 ms
+
+Description:
+Paste problem statement here
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define SPEED ios::sync_with_stdio(false);cin.tie(0); cout.tie(0);
+typedef pair<int, int> pii;
+
+void solve() 
+{
+    int n;
+    cin>>n;
+    if(n % 2 == 0)
+    {
+        cout<<"Yes\n";
+        cout<<n/2<<" "<<n/2<<"\n";
+    }
+    else
+    {
+        cout<<"No\n";
+    }
+}
+
+signed main() 
+{
+    SPEED;
+    int t = 1;
+    cin >> t;
+    while(t--) 
+    {
+        solve();
+    }
+    return 0;
+}
