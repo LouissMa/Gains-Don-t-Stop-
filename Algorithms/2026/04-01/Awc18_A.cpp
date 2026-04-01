a1a1b65ba46303ce1b2381830c08c49ade552689
LouissMa <634688345@qq.com>
2026-04-01 22:55:00 +0800

    Add Awc18 Problem A solution for 2026-04-01

---
+++ Algorithms/2026/04-01/Awc18_A.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define SPEED ios::sync_with_stdio(false);cin.tie(0); cout.tie(0);
+typedef pair<int, int> PII;
+
+signed main()
+{
+    SPEED;
+    int n,m;
+    cin>>n>>m;
+    cout<<m * (2 * (n - 1))<<"\n";
+    return 0;
+}
