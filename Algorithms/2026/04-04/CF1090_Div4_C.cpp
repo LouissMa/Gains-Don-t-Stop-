75e6841e325ed84faedbffccee5b15aa375cdcf8
LouissMa <634688345@qq.com>
2026-04-04 23:10:25 +0800

    Add Codeforces 1090 Div4 Problem C solution

---
+++ Algorithms/2026/04-04/CF1090_Div4_C.cpp
@@ -0,0 +1,39 @@
+/*
+Problem: Problem Title
+Contest: Contest Name
+URL: URL
+Memory Limit: 1024 MB
+Time Limit: 2000 ms
+
+Description:
+Paste problem statement here
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define SPEED ios::sync_with_stdio(false);cin.tie(0); cout.tie(0);
+typedef pair<int, int> pii;
+
+void solve() 
+{
+    int n;
+    cin>>n;
+    for(int i=1;i<=n;i++)
+    {
+        cout<<i<<" "<<n + 2 * i - 1<<" "<<n + 2 * i<<" ";
+    }
+    cout<<endl;
+}
+
+signed main() 
+{
+    SPEED;
+    int t = 1;
+    cin >> t;
+    while(t--) 
+    {
+        solve();
+    }
+    return 0;
+}
