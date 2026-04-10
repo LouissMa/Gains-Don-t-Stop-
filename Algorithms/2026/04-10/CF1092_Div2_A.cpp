75ddada346b3f31104857cd3eff12416f59685d2
LouissMa <634688345@qq.com>
2026-04-10 16:24:00 +0800

    Add Codeforces 1092 Div2 Problem A solution

---
+++ Algorithms/2026/04-10/CF1092_Div2_A.cpp
@@ -0,0 +1,43 @@
+/*
+Problem: Problem Title
+Contest: Contest Name
+URL: URL
+Memory Limit: 1024 MB
+Time Limit: 2000 ms
+
+Description:
+Paste problem statement here
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define SPEED ios::sync_with_stdio(false);cin.tie(0); cout.tie(0);
+typedef pair<int, int> pii;
+
+void solve() 
+{
+    int n,k;
+    cin>>n>>k;
+    int s = 0;
+    for(int i = 0;i<n;i++)
+    {
+        int x;
+        cin>>x;
+        s += x;
+    }
+    if(s % 2 == 1 || (n * k) % 2 == 0) cout<<"YES\n";
+    else cout<<"NO\n";
+}
+
+signed main() 
+{
+    SPEED;
+    int t = 1;
+    cin >> t;
+    while(t--) 
+    {
+        solve();
+    }
+    return 0;
+}
