b374301406ea87df6259e1b15ebdf7139f500782
LouissMa <634688345@qq.com>
2026-04-13 21:55:43 +0800

    Add AtCoder Beginner Contest 402 Problem D solution

---
+++ Algorithms/2026/04-13/ABC402_D.cpp
@@ -0,0 +1,25 @@
+/*
+Problem: Line Crossing
+Contest: Tokio Marine & Nichido Fire Insurance Programming Contest 2025 (AtCoder Beginner Contest 402)
+URL: https://atcoder.jp/contests/abc402/tasks/abc402_d
+Memory Limit: 1024 MB
+Time Limit: 2000 ms
+
+Description:
+Paste problem statement here
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define SPEED ios::sync_with_stdio(false);cin.tie(0); cout.tie(0);
+typedef pair<int, int> PII;
+
+const int N = 3e5 + 10;
+signed main()
+{
+    SPEED;
+    int n,m;
+    cin>>n>>m;
+    int ans = 0;
+}
