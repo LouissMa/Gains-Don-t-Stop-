8fcef52d7f69f8a05b8a3ea20e62ec6343f4b1d3
LouissMa <634688345@qq.com>
2026-04-22 21:40:36 +0800

    Add Codeforces 965 Div2 Problem B solution

---
+++ Algorithms/2026/04-22/CF965_Div2_B.cpp
@@ -0,0 +1,39 @@
+/*
+Problem: Minimize Equal Sum Subarrays
+Memory Limit: 1024 MB
+Time Limit: 2000 ms
+
+Description:
+Paste problem statement here
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define SPEED ios::sync_with_stdio(false);cin.tie(0); cout.tie(0);
+typedef pair<int, int> pii;
+
+void solve() 
+{
+    int n;
+    cin>>n;
+    vector<int> p(n);
+    for(int i=0;i<n;i++) cin>>p[i];
+    rotate(p.begin(),p.begin()+1,p.end());
+    for (int i = 0; i < n; i++) 
+    {
+        std::cout << p[i] << " \n"[i == n - 1];
+    }
+}
+
+signed main() 
+{
+    SPEED;
+    int t = 1;
+    cin >> t;
+    while(t--) 
+    {
+        solve();
+    }
+    return 0;
+}
